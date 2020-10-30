569486d183e3016b176acca37ad48523ce0fb3f9
valmirt <valmirt.dev@gmail.com>
2020-10-29 21:53:28 -0300

    last commit

--- NoPonto WatchKit Extension/InterfaceController.swift
+++ NoPonto WatchKit Extension/InterfaceController.swift
@@ -32,7 +32,7 @@ final class InterfaceController: WKInterfaceController {
     //MARK: - SuperMethods
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        //        groupImage.setHidden(true)
+        groupImage.setHidden(true)
         setupPickers()
         updateConfiguration()
     }
@@ -53,7 +53,7 @@ final class InterfaceController: WKInterfaceController {
         var weightItems: [WKPickerItem] = []
         for weight in stride(from: 0.1, through: maxKg, by: increment) {
             let item = WKPickerItem()
-            item.title = weight.description
+            item.title = String(format: "%0.1f", weight)
             weightItems.append(item)
         }
         
@@ -72,7 +72,13 @@ final class InterfaceController: WKInterfaceController {
     }
     
     func updateConfiguration() {
-        
+        labelCook.setText(meatTemperature.stringValue)
+        labelTemperature.setText(meatTemperature.stringValue)
+        pickerTemperature.setSelectedItemIndex(meatTemperature.rawValue)
+        sliderCook.setValue(Float(meatTemperature.rawValue))
+        labelWeight.setText("Total: \(String(format: "%0.1f", kg)) kg")
+        let index = Int((kg * 10) - 1)
+        pickerWeight.setSelectedItemIndex(index)
     }
     
     //MARK: - IBActions
@@ -93,6 +99,7 @@ final class InterfaceController: WKInterfaceController {
     @IBAction func toggleTimer() {
         if timeRunning {
             timer.stop()
+            timer.setDate(Date(timeIntervalSinceNow: 0))
             buttonTimer.setTitle("Iniciar Timer")
         } else {
             let time = meatTemperature.cookTime(for: kg)
@@ -104,14 +111,26 @@ final class InterfaceController: WKInterfaceController {
     }
     
     @IBAction func onSliderChange(_ value: Float) {
+        if let newMeatTemperature = MeatTemperature(rawValue: Int(value)) {
+            meatTemperature = newMeatTemperature
+            updateConfiguration()
+        }
     }
     
     @IBAction func onPickerWeightChange(_ value: Int) {
+        kg = Double(value + 1) * increment
+        updateConfiguration()
     }
     
     @IBAction func onPickerTemperatureChange(_ value: Int) {
+        if let newMeatTemperature = MeatTemperature(rawValue: value) {
+            meatTemperature = newMeatTemperature
+            updateConfiguration()
+        }
     }
     
     @IBAction func onModeChange(_ value: Bool) {
+        groupImage.setHidden(!value)
+        groupText.setHidden(value)
     }
 }
